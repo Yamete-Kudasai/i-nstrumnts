58127f2464ceb0c3233019319fa2bcd4580fb3fc
AnaMaria-Sole <anamgarcia.a@yahoo.com>
2024-07-19 18:39:58 +0200

    coming-soon page updated with id for js and dinamic js for js file

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const params = new URLSearchParams(window.location.search);
+    const page = params.get('page');
+
+    // Este es el contenido para cada página
+    const images = {
+        '1': '../public/assets/img/saxophone-with-background.png',
+        '2': '../public/assets/img/guitar-with-background.png',
+        '3': '../public/assets/img/piano-with-background.png'
+    };
+
+    // Aquí cambia la imagen de la página según el instrumento escogido
+    if (images[page]) {
+        document.getElementById('saxophone-image').src = images[page];
+    }
+});
