386768f1ebbb6351c03e19e5fa1aa0605f1b5860
Smith Grisales <developers.smith@gmail.com>
2024-07-20 23:46:20 +0200

    create first drums, integrate of animations keypress

--- src/js/app.js
+++ src/js/app.js
@@ -16,12 +16,18 @@ function openMenu() {
 // INSTRUMENTO
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    
+    
+
+
     const drums = document.querySelectorAll('.drum-part');
 
     drums.forEach(drum => {
         drum.addEventListener('mousedown', () => {
             const sound = drum.getAttribute('data-sound');
             playSound(sound);
+            
         });
     });
 
@@ -30,14 +36,55 @@ document.addEventListener('DOMContentLoaded', () => {
         audio.play();
     }
     
-    document.addEventListener('keydown', (event) => {
-        const key = event.key.toLowerCase();
-        const drum = document.querySelector(`.drum-part[data-key="${key}"]`);
+    document.addEventListener('keyup', (event) => {
+        var key = event.key.toLowerCase();
+        var drum = document.querySelector(`.drum-part[data-key="${key}"]`);
         if (drum) {
             const sound = drum.getAttribute('data-sound');
             playSound(sound);
+
+            
         }
     });
+
+    let keypressed = new Set();
+            document.addEventListener('keydown',(e) =>{
+                let  keypress = e.key
+                keypressed.add(keypress)
+                opacityDrum()
+            });
+
+            document.addEventListener('keyup',(e) =>{
+                let  keypressRelay = e.key
+                keypressed.delete(keypressRelay)
+                opacityDrum()
+            });
+
+
+
+            function opacityDrum(){
+                
+                const parts = document.querySelectorAll('.drum-part'); // Selecciona todos los elementos con la clase 'btn-drum'
+            
+                parts.forEach(part => {
+        
+                    let key = part.getAttribute('data-key')
+                    if (keypressed.has(key)) {
+                        part.classList.remove('opacity-1');
+                        part.classList.add('opacity-75');
+                    } else {
+                        part.classList.remove('opacity-75');
+                        part.classList.add('opacity-1');
+                    }
+                });
+            }
+
+
+
+    const volumeController = document.getElementById("volumecontroller")
+    volumeController.addEventListener("input", () => {
+        HTMLMediaElement.volume=volumeController.value/100
+    })
 });
 
 //FIN INSTRUMENTO
